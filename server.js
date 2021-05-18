0d1a298fcf637cdd90ac6921d3a6669f20866526
Shilohjones194 <76697212+Shilohjones194@users.noreply.github.com>
2021-05-18 10:43:00 -0700

    completed 11.1

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 const { animals } = require('./data/animals.json');
@@ -43,11 +44,23 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
 }
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id) [0];
+    return result;
+}
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);  
+    } else {
+        res.send(404);
+    }
 });
 
